85a1d15f3d65370653d8fece6d5b68e9be3e07f9
Michael P <pomp@schapo-design.de>
2015-07-16 17:44:33 +0200

    Added addon jar for Openhab

--- src/main/java/org/openhab/binding/pca9685control/internal/pca9685controlGenericBindingProvider.java
+++ src/main/java/org/openhab/binding/pca9685control/internal/pca9685controlGenericBindingProvider.java
@@ -15,6 +15,7 @@ import org.openhab.core.binding.BindingConfig;
 import org.openhab.core.items.Item;
 import org.openhab.core.library.items.DimmerItem;
 import org.openhab.core.library.items.NumberItem;
+import org.openhab.core.library.items.StringItem;
 import org.openhab.core.library.items.SwitchItem;
 import org.openhab.core.library.types.PercentType;
 import org.openhab.model.item.binding.AbstractGenericBindingProvider;
